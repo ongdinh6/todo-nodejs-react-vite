bb5f5cbf3cba61de6aaf02c0d71c8c8d83c29221
ongdinh6 <ongdinh6@gmail.com>
2024-04-21 00:02:32 +0700

    04212024 | Update package.json and config unit testing for backend

---
+++ packages/backend/__test__/index.test.ts
@@ -0,0 +1,23 @@
+import request from "supertest";
+import app from "@/index.ts";
+
+describe("Express App", () => {
+  it("responds to /api/hello", async () => {
+    const response = await request(app).get("/api/hello");
+    expect(response.status).toBe(200);
+    expect(response.text).toEqual("Hello world!");
+  });
+
+  it("responds to /api/hello with name query parameter", async () => {
+    const response = await request(app).get("/api/hello?name=John");
+    expect(response.status).toBe(200);
+    expect(response.text).toEqual("Hello John");
+  });
+
+  it("responds to non-existing routes when running locally", async () => {
+    const response = await request(app).get("/non-existing-route");
+    expect(response.status).toBe(404);
+  });
+
+  // Add more test cases for other routes and edge cases
+});
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
-import todoRoutes from "./controllers/todoController";
+import todoRoutes from "@controllers/todoController";
 
 const app = express();
 const port = 8080;
@@ -25,7 +25,7 @@ app.get("/api/hello", (req, res) => {
 
 const isLocal = true; // replaced by env
 if (isLocal) {
-  app.get("/*", (req, res) => {
+  app.get("/*", (_req, res) => {
     res.sendFile(staticDir + "/index.html");
   });
 }
@@ -33,3 +33,5 @@ if (isLocal) {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+export default app;
